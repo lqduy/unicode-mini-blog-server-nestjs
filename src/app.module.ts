452cc46b0d40ef466af82d3f22dd39efa02a1aec
lqduy <lqduycp@gmail.com>
2025-01-12 10:24:54 +0700

    feat(posts): implement basic CRUD functionality for posts module
    Adds the Posts module, including the controller and service for 
    handling CRUD operations. Introduces Post and CreatePostDto 
    entities, and integrates the PostsModule into the main app 
    module. Updates package dependencies to include 
    @nestjs/mapped-types for DTO handling.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 
+import { PostsModule } from "./modules/posts/posts.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
